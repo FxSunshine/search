fd5477811023c750147563182d79b7ea6160cf77
xuxj <hugeinfo123>
2019-10-31 16:58:54 +0800

    Lucene 能力集成

--- src/main/java/com/sjar/search/config/LuceneConfig.java
+++ src/main/java/com/sjar/search/config/LuceneConfig.java
@@ -1,14 +1,8 @@
 package com.sjar.search.config;
 
-import org.apache.lucene.analysis.Analyzer;
-import org.apache.lucene.analysis.cn.smart.SmartChineseAnalyzer;
-import org.apache.lucene.index.IndexWriter;
-import org.apache.lucene.index.IndexWriterConfig;
-import org.apache.lucene.search.ControlledRealTimeReopenThread;
-import org.apache.lucene.search.SearcherFactory;
-import org.apache.lucene.search.SearcherManager;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,22 +11,18 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+/**
+ * @author xuxiangjian
+ */
 @Configuration
 public class LuceneConfig {
     /**
      * lucene索引,存放位置
+     * @author xuxiangjian
+     * @since 1.0.0
      */
-    private static final String LUCENE_INDEX_PATH="/Users/xuxiangjian/大数据学习/files/Lucene";
-    /**
-     * 创建一个 Analyzer 实例
-     *
-     * @return
-     */
-    @Bean
-    public Analyzer analyzer() {
-        return new SmartChineseAnalyzer();
-    }
-
+    @Value("${lucene.index.path:}")
+    private  String LUCENE_INDEX_PATH;
     /**
      * 索引位置
      *
@@ -51,42 +41,5 @@ public class LuceneConfig {
         return FSDirectory.open(path);
     }
 
-    /**
-     * 创建indexWriter
-     *
-     * @param directory
-     * @param analyzer
-     * @return
-     * @throws IOException
-     */
-    @Bean
-    public IndexWriter indexWriter(Directory directory, Analyzer analyzer) throws IOException {
-        IndexWriterConfig indexWriterConfig = new IndexWriterConfig(analyzer);
-        IndexWriter indexWriter = new IndexWriter(directory, indexWriterConfig);
-        // 清空索引
-        indexWriter.deleteAll();
-        indexWriter.commit();
-        return indexWriter;
-    }
-
-    /**
-     * SearcherManager管理
-     *
-     * @param directory
-     * @return
-     * @throws IOException
-     */
-    @Bean
-    public SearcherManager searcherManager(Directory directory, IndexWriter indexWriter) throws IOException {
-        SearcherManager searcherManager = new SearcherManager(indexWriter, false, false, new SearcherFactory());
-        ControlledRealTimeReopenThread cRTReopenThead = new ControlledRealTimeReopenThread(indexWriter, searcherManager,
-                5.0, 0.025);
-        cRTReopenThead.setDaemon(true);
-        //线程名称
-        cRTReopenThead.setName("更新IndexReader线程");
-        // 开启线程
-        cRTReopenThead.start();
-        return searcherManager;
-    }
 }
 
---
+++ src/main/java/com/sjar/search/lucene/LuceneService.java
@@ -0,0 +1,161 @@
+package com.sjar.search.lucene;
+
+import com.google.protobuf.ServiceException;
+import com.sjar.search.config.LuceneConfig;
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.index.*;
+import org.apache.lucene.queryparser.classic.MultiFieldQueryParser;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.*;
+import org.apache.lucene.store.Directory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.wltea.analyzer.lucene.IKAnalyzer;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author xuxiangjian
+ */
+public class LuceneService {
+
+    private static Logger logger = LoggerFactory.getLogger(LuceneService.class);
+
+    public static final String PRIMARY_KEY = "id";
+
+    @Autowired
+    private LuceneConfig luceneConfig;
+    /**
+     * 创建索引
+     * @author xuxiangjian
+     * @since 1.0.0
+     * @param indexMaps
+     * @throws Exception
+     */
+    public void createIndex(Map<String,Object> indexMaps) throws Exception {
+
+        Directory dir = luceneConfig.directory();
+        //分词器
+        Analyzer analyzer = new IKAnalyzer();
+        IndexWriterConfig indexWriterConfig = new IndexWriterConfig(analyzer);
+        indexWriterConfig.setOpenMode(IndexWriterConfig.OpenMode.CREATE_OR_APPEND);
+        IndexWriter writer = new IndexWriter(dir, indexWriterConfig);
+        //先删除
+        if (indexMaps.get(PRIMARY_KEY) != null) {
+            writer.deleteDocuments(new Term(PRIMARY_KEY, indexMaps.get(PRIMARY_KEY).toString()));
+        }else {
+            throw new Exception("主键的key必须为id,并且值不能为空");
+        }
+        Document doc = new Document();
+        indexMaps.forEach((key, value) -> {
+            doc.add(new StringField(key, value.toString(), Field.Store.YES));
+        });
+        writer.addDocument(doc);
+        writer.commit();
+        //关闭
+        writer.close();
+        dir.close();
+    }
+
+    /**
+     * 删除索引
+     * @author xuxiangjian
+     * @since 1.0.0
+     * @param indexKey 为创建索引时定义的索引key
+     * @param primaryKey 为业务的主键key
+     */
+    public void delete(String indexKey,String primaryKey) {
+        try {
+            Directory dir = luceneConfig.directory();
+            //分词器
+            Analyzer analyzer = new IKAnalyzer();
+            IndexWriterConfig indexWriterConfig = new IndexWriterConfig(analyzer);
+            IndexWriter writer = new IndexWriter(dir, indexWriterConfig);
+            writer.deleteDocuments(new Term(indexKey, primaryKey));
+            writer.close();
+            dir.close();
+        } catch (IOException e) {
+            logger.error(e.getMessage(), e);
+        }
+    }
+
+    /**
+     * 根据关键字查询索引
+     * @author xuxiangjian
+     * @since 1.0.0
+     * @param keyword
+     * @param fieldQuery
+     * @return
+     * @throws ServiceException
+     */
+    public List<Document> find(String keyword,String[] fieldQuery) throws ServiceException {
+        try {
+            Directory dir = luceneConfig.directory();
+            IndexReader indexReader = DirectoryReader.open(dir);
+            IndexSearcher indexSearcher = new IndexSearcher(indexReader);
+
+            //分词器
+            Analyzer analyzer = new IKAnalyzer();
+            // 在title与content中查询
+            QueryParser parser = new MultiFieldQueryParser(fieldQuery,analyzer);
+            Query query = parser.parse(keyword);
+            System.out.println("Query分词 = " + query);
+
+            List<Document> documents = this.query(indexSearcher, query);
+            indexReader.close();
+            dir.close();
+            return documents;
+        }catch (IndexNotFoundException e1){
+            return null;
+        }catch (Exception e) {
+            logger.error(e.getMessage(), e);
+            throw new ServiceException("根据关键字，查询出错！", e);
+        }
+    }
+
+
+    /**
+     * 索引文档查询
+     * @author xuxiangjian
+     * @since 1.0.0
+     * @param searcher
+     * @param query
+     * @return
+     */
+    public List<Document> query(IndexSearcher searcher,Query query) {
+        TopDocs topDocs = null;
+        ScoreDoc[] scores =null;
+        int length = 0;
+        try {
+            topDocs = searcher.search(query, Integer.MAX_VALUE);
+        } catch (IOException e) {
+            logger.error(e.getMessage(),e);
+        }
+        if (topDocs != null) {
+            scores = topDocs.scoreDocs;
+        }
+        if (scores != null) {
+            length = scores.length;
+        }
+        if (length <= 0) {
+            return Collections.emptyList();
+        }
+        List<Document> docList = new ArrayList<Document>();
+        try {
+            for (int i = 0; i < length; i++) {
+                Document doc = searcher.doc(scores[i].doc);
+                docList.add(doc);
+            }
+        } catch (IOException e) {
+            logger.error(e.getMessage(),e);
+        }
+        return docList;
+    }
+}
